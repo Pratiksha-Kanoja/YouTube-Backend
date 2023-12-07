9c2742d4dcce8dc06486108095c82a6badcd037c
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-12-08 03:32:53 +0530

    new

--- Routes/Auth.routes.js
+++ Routes/Auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { Login, Registeration } from "../Controllers/Auth.controller.js";
+import { Login, Registeration, getCurrentUser } from "../Controllers/Auth.controller.js";
 
 const authrouter = Router();
 
 authrouter.post('/register',Registeration);
 authrouter.post('/login',Login);
+authrouter.post('/get-current-user',getCurrentUser);
 
 export default authrouter;
